5be49df22242c5236fc1f4297b6b474eb73c1237
nicolas de Barquin <nicolas@openfab.be>
2022-09-12 14:23:23 +0200

    working read and update readme

--- README.md
+++ README.md
@@ -3,15 +3,12 @@
 Project forked to explore how to use this board.   
 
 ### This program
-`main.cpp` : With I2C communication, each slave on the bus has its own address, a hexadecimal number that allows the ESP32 to communicate with each device.  
-This code will scan the I2C BUS and return your devices’ I2C address.  
+`main.cpp` : 
 
 > For ARDUINO IDE: You can copy/paste the code from `main.cpp` Don't forget to install libraries. 
 
 #### Notes:
-- M5Stamp C3U can use any GPIO as I2C BUS. You need to define the pin you want to use with `Wire.begin(I2C_SDA, I2C_SCL);`   
-I have tested on (1,0) and (6,7) _(sda, sdc)  
-- But only have 1 BUS I2C. _(ESP32 can manage 2 BUS I2C)_   
-Check this doc: [Multiple I2C devices (same bus, different addresses)](https://randomnerdtutorials.com/esp32-i2c-communication-arduino-ide/#5)
+- I'm using this library : https://github.com/RobTillaart/AM232X
+- Still need to add in setup `Wire.begin(6,7);`
 
-For more I2C stuff, check [issue #2](https://github.com/nicolasdb/M5Stamp-C3U/issues/2)
+For more AM2320 stuff, check [issue #5](https://github.com/nicolasdb/M5Stamp-C3U/issues/5)
--- src/main.cpp
+++ src/main.cpp
@@ -1,87 +1,83 @@
-// I2C device class (I2Cdev) demonstration Arduino sketch for ADXL345 class
-// 10/7/2011 by Jeff Rowberg <jeff@rowberg.net>
-// Updates should (hopefully) always be available at https://github.com/jrowberg/i2cdevlib
 //
-// Changelog:
-//     2011-10-07 - initial release
+//    FILE: AM2320_test.ino
+//  AUTHOR: Rob Tillaart
+// PURPOSE: demo sketch for AM2320 I2C humidity & temperature sensor
+//
+//  AM232X PIN layout             AM2315 COLOR
+//  ============================================
+//   bottom view  DESCRIPTION     COLOR
+//       +---+
+//       |o  |       VDD          RED
+//       |o  |       SDA          YELLOW
+//       |o  |       GND          BLACK
+//       |o  |       SCL          GREY
+//       +---+
+//
+// do not forget pull up resistors between SDA, SCL and VDD.
 
-/* ============================================
-I2Cdev device library code is placed under the MIT license
-Copyright (c) 2011 Jeff Rowberg
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in
-all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-THE SOFTWARE.
-===============================================
-*/
-
-// Arduino Wire library is required if I2Cdev I2CDEV_ARDUINO_WIRE implementation
-// is used in I2Cdev.h
 #include "Arduino.h"
 #include "Wire.h"
+#include "AM232X.h"
 
-// I2Cdev and ADXL345 must be installed as libraries, or else the .cpp/.h files
-// for both classes must be in the include path of your project
-#include "I2Cdev.h"
-#include "ADXL345.h"
+AM232X AM2320;
 
-// class default I2C address is 0x53
-// specific I2C addresses may be passed as a parameter here
-// ALT low = 0x53 (default for SparkFun 6DOF board)
-// ALT high = 0x1D
-ADXL345 accel;
 
-int16_t ax, ay, az;
+void setup()
+{
+  Serial.begin(115200);
+  Wire.begin(6,7);
+  Serial.println(__FILE__);
+  Serial.print("LIBRARY VERSION: ");
+  Serial.println(AM232X_LIB_VERSION);
+  Serial.println();
 
-#define LED_PIN 13 // (Arduino is 13, Teensy is 6)
-bool blinkState = false;
+  if (! AM2320.begin() )
+  {
+    Serial.println("Sensor not found");
+    while (1);
+  }
+  AM2320.wakeUp();
+  delay(2000);
 
-void setup() {
-    // join I2C bus (I2Cdev library doesn't do this automatically)
-    Wire.begin(6,7);
+  // sensor only returns one decimal.
+  Serial.println();
+  Serial.println("Read:\t");
 
-    // initialize serial communication
-    // (38400 chosen because it works as well at 8MHz as it does at 16MHz, but
-    // it's really up to you depending on your project)
-    Serial.begin(115200);
+  int status = AM2320.read();
+  switch (status)
+  {
+    case AM232X_OK:
+      Serial.println("OK");
+      break;
+    default:
+      Serial.println(status);
+      break;
+  }
 
-    // initialize device
-    Serial.println("Initializing I2C devices...");
-    accel.initialize();
 
-    // verify connection
-    Serial.println("Testing device connections...");
-    Serial.println(accel.testConnection() ? "ADXL345 connection successful" : "ADXL345 connection failed");
-
-    // configure LED for output
-    pinMode(LED_PIN, OUTPUT);
+  Serial.println("done...\n");
 }
 
-void loop() {
-    // read raw accel measurements from device
-    accel.getAcceleration(&ax, &ay, &az);
 
-    // display tab-separated accel x/y/z values
-    Serial.print("accel:\t");
-    Serial.print(ax); Serial.print("\t");
-    Serial.print(ay); Serial.print("\t");
-    Serial.println(az);
+void loop()
+{
+  delay(2000);
 
-    // blink LED to indicate activity
-    blinkState = !blinkState;
-    digitalWrite(LED_PIN, blinkState);
+  int status = AM2320.read();
+  switch (status)
+  {
+    case AM232X_OK:
+      Serial.println("OK");
+      break;
+    default:
+      Serial.println(status);
+      break;
+  }
+  Serial.print("Temp:\t");
+  Serial.println(AM2320.getTemperature(), 1);
+  Serial.print("Hum:\t");
+  Serial.println(AM2320.getHumidity(), 1);
 }
+
+
+// -- END OF FILE --
