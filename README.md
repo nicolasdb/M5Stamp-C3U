5656ac09a0fdbbaa556de578e620a5087d4480e9
nicolas de Barquin <nicolas@openfab.be>
2022-09-12 17:49:32 +0200

    RTC working + readme update

--- README.md
+++ README.md
@@ -3,11 +3,13 @@
 Project forked to explore how to use this board.   
 
 ### This program
-`main.cpp` : WIP
+`main.cpp` : :warning: Work in progress  
+The sketch will give you complete understanding on how to set/read date & time on DS1307 RTC module and can serve as the basis for more practical experiments and projects.
 
 > For ARDUINO IDE: You can copy/paste the code from `main.cpp` Don't forget to install libraries. 
 
 #### Notes:
+- I followed part oh [this tutorial](https://lastminuteengineers.com/ds1307-rtc-arduino-tutorial/)
+- I needed to include SPI.h
 
-
-For more ADXL345 stuff, check [issue #4](https://github.com/nicolasdb/M5Stamp-C3U/issues/4)
+For more Data Logger stuff, check [issue #6](https://github.com/nicolasdb/M5Stamp-C3U/issues/6)
--- src/main.cpp
+++ src/main.cpp
@@ -1,87 +1,83 @@
-// I2C device class (I2Cdev) demonstration Arduino sketch for ADXL345 class
-// 10/7/2011 by Jeff Rowberg <jeff@rowberg.net>
-// Updates should (hopefully) always be available at https://github.com/jrowberg/i2cdevlib
-//
-// Changelog:
-//     2011-10-07 - initial release
-
-/* ============================================
-I2Cdev device library code is placed under the MIT license
-Copyright (c) 2011 Jeff Rowberg
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in
-all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-THE SOFTWARE.
-===============================================
-*/
-
-// Arduino Wire library is required if I2Cdev I2CDEV_ARDUINO_WIRE implementation
-// is used in I2Cdev.h
 #include "Arduino.h"
-#include "Wire.h"
+#include <Wire.h>
+#include "RTClib.h"
+#include <SPI.h>
 
-// I2Cdev and ADXL345 must be installed as libraries, or else the .cpp/.h files
-// for both classes must be in the include path of your project
-#include "I2Cdev.h"
-#include "ADXL345.h"
+#define I2C_SDA 1
+#define I2C_SCL 0
 
-// class default I2C address is 0x53
-// specific I2C addresses may be passed as a parameter here
-// ALT low = 0x53 (default for SparkFun 6DOF board)
-// ALT high = 0x1D
-ADXL345 accel;
+RTC_DS1307 rtc;
 
-int16_t ax, ay, az;
+char daysOfTheWeek[7][12] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
 
-#define LED_PIN 13 // (Arduino is 13, Teensy is 6)
-bool blinkState = false;
+void setup () 
+{
+  Serial.begin(115200);
+  Wire.begin(I2C_SDA,I2C_SCL);
+  delay(3000); // wait for console opening
 
-void setup() {
-    // join I2C bus (I2Cdev library doesn't do this automatically)
-    Wire.begin(1,0);
+  if (! rtc.begin()) {
+    Serial.println("Couldn't find RTC");
+    while (1);
+  }
 
-    // initialize serial communication
-    // (38400 chosen because it works as well at 8MHz as it does at 16MHz, but
-    // it's really up to you depending on your project)
-    Serial.begin(115200);
-
-    // initialize device
-    Serial.println("Initializing I2C devices...");
-    accel.initialize();
-
-    // verify connection
-    Serial.println("Testing device connections...");
-    Serial.println(accel.testConnection() ? "ADXL345 connection successful" : "ADXL345 connection failed");
-
-    // configure LED for output
-    pinMode(LED_PIN, OUTPUT);
+  if (!rtc.isrunning()) {
+    Serial.println("RTC lost power, lets set the time!");
+	
+	// Comment out below lines once you set the date & time.
+    // Following line sets the RTC to the date & time this sketch was compiled
+    rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+	
+    // Following line sets the RTC with an explicit date & time
+    // for example to set January 27 2017 at 12:56 you would call:
+    // rtc.adjust(DateTime(2017, 1, 27, 12, 56, 0));
+  }
 }
 
-void loop() {
-    // read raw accel measurements from device
-    accel.getAcceleration(&ax, &ay, &az);
-
-    // display tab-separated accel x/y/z values
-    Serial.print("accel:\t");
-    Serial.print(ax); Serial.print("\t");
-    Serial.print(ay); Serial.print("\t");
-    Serial.println(az);
-
-    // blink LED to indicate activity
-    blinkState = !blinkState;
-    digitalWrite(LED_PIN, blinkState);
+void loop () 
+{
+    DateTime now = rtc.now();
+    
+    Serial.println("Current Date & Time: ");
+    Serial.print(now.year(), DEC);
+    Serial.print('/');
+    Serial.print(now.month(), DEC);
+    Serial.print('/');
+    Serial.print(now.day(), DEC);
+    Serial.print(" (");
+    Serial.print(daysOfTheWeek[now.dayOfTheWeek()]);
+    Serial.print(") ");
+    Serial.print(now.hour(), DEC);
+    Serial.print(':');
+    Serial.print(now.minute(), DEC);
+    Serial.print(':');
+    Serial.print(now.second(), DEC);
+    Serial.println();
+    
+    Serial.println("Unix Time: ");
+    Serial.print("elapsed ");
+    Serial.print(now.unixtime());
+    Serial.print(" seconds/");
+    Serial.print(now.unixtime() / 86400L);
+    Serial.println(" days since 1/1/1970");
+    
+    // calculate a date which is 7 days & 30 seconds into the future
+    DateTime future (now + TimeSpan(7,0,0,30));
+    
+    Serial.println("Future Date & Time (Now + 7days & 30s): ");
+    Serial.print(future.year(), DEC);
+    Serial.print('/');
+    Serial.print(future.month(), DEC);
+    Serial.print('/');
+    Serial.print(future.day(), DEC);
+    Serial.print(' ');
+    Serial.print(future.hour(), DEC);
+    Serial.print(':');
+    Serial.print(future.minute(), DEC);
+    Serial.print(':');
+    Serial.print(future.second(), DEC);
+    Serial.println();
+    
+    Serial.println();
+    delay(1000);
 }
