6c319c1a96926ae5b3b92f8e45f5a59eb597f5be
nicolas de Barquin <nicolas@openfab.be>
2022-09-28 13:03:04 +0200

    working

--- README.md
+++ README.md
@@ -2,19 +2,45 @@
 
 Project forked to explore how to use this board.   
 
-### This program
-`main.cpp` : :warning: Work in progress  
-The sketch will define functions to list, write, append and read a file on the SPIFFS partition.   
-Then using a simple timer to read a sensor and add the value in a log.txt file. 
-Pushing the button on M5stamp will print the file on serial monitor. 
+### M5Stamp C3U as an ACCESS POINT
+`main.cpp` : 
+The sketch will set the ESP32C3 as an access point, then a webserver is use to control ON/OFF two leds connected on the MCP23017 on my setup.  
+
 
 > For ARDUINO IDE: You can copy/paste the code from `main.cpp` Don't forget to install libraries. 
 
 #### Notes:
-- the setup in Arduino IDE was quite straight forward, but I was stuck in Pio with a problem to mount the partition.  
-This was solved with the package update to 2.0.5.  
-`platform_packages = 
-	framework-arduinoespressif32 @ https://github.com/espressif/arduino-esp32.git#2.0.5`
+- Work with any web servers application, the important part is to replace this:
+```
+// Connect to Wi-Fi network with SSID and password
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  // Print local IP address and start web server
+  Serial.println("");
+  Serial.println("WiFi connected.");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+```
+
+with this, to enable soft Access Point
+```  // Connect to Wi-Fi network with SSID and password
+  Serial.println("Setting AP (Access Point)…");
+  // Remove the password parameter, if you want the AP (Access Point) to be open
+  WiFi.softAP(ssid, password);
+  
+  // Print AccessPoint IP address and start web server
+  IPAddress IP = WiFi.softAPIP();
+  Serial.print("Page on this IP address: ");
+  Serial.println(IP);
+```
+
+- PlateformIO need time to start monitor. Add enough delay in `void setup ()` before the first `Serial.print()`  
+  I didn't had the problem in Arduino IDE.  
 
 
-For more SPIFFS stuff, check [issue #7](https://github.com/nicolasdb/M5Stamp-C3U/issues/7)
+Check [issue #9](https://github.com/nicolasdb/M5Stamp-C3U/issues/9)
--- src/main.cpp
+++ src/main.cpp
@@ -1,142 +1,164 @@
-#include <SPIFFS.h>
-#include <SSVTimer.h>
-#include <FS.h>
+/*********
+  Rui Santos
+  Complete project details at http://randomnerdtutorials.com  
+*********/
 
-String dataMessage;
+// Load Wi-Fi library
+#include "Arduino.h"
+#include <WiFi.h>
+#include <Wire.h>                    // controle I2C
+#include <Adafruit_MCP23X17.h>       // controle MCP
 
+#define I2C_Freq 100000UL 
+#define SDA_1 1                      
+#define SCL_1 0
+Adafruit_MCP23X17 mcp;
 
-// SPIFFS functions
-#pragma region        // to shrink SPIFFS lines in VS code
+// Replace with your network credentials
+const char* ssid = "EAsy-Access-Point";
+const char* password = "1234";
 
-#define FORMAT_SPIFFS_IF_FAILED true
+// Set web server port number to 80
+WiFiServer server(80);
 
-void listDir(fs::FS &fs, const char * dirname, uint8_t levels){
-    Serial.printf("Listing directory: %s\r\n", dirname);
+// Variable to store the HTTP request
+String header;
 
-    File root = fs.open(dirname);
-    if(!root){
-        Serial.println("- failed to open directory");
-        return;
-    }
-    if(!root.isDirectory()){
-        Serial.println(" - not a directory");
-        return;
-    }
+// Auxiliar variables to store the current output state
+String outputmcp0State = "off";
+String outputmcp1State = "off";
 
-    File file = root.openNextFile();
-    while(file){
-        if(file.isDirectory()){
-            Serial.print("  DIR : ");
-            Serial.println(file.name());
-            if(levels){
-                listDir(fs, file.path(), levels -1);
-            }
-        } else {
-            Serial.print("  FILE: ");
-            Serial.print(file.name());
-            Serial.print("\tSIZE: ");
-            Serial.println(file.size());
-        }
-        file = root.openNextFile();
-    }
-}
+// Assign output variables to GPIO pins
+const int outputmcp0 = 0;
+const int outputmcp1 = 1;
 
-void readFile(fs::FS &fs, const char * path){
-    Serial.printf("Reading file: %s\r\n", path);
-
-    File file = fs.open(path);
-    if(!file || file.isDirectory()){
-        Serial.println("- failed to open file for reading");
-        return;
-    }
-
-    Serial.println("- read from file:");
-    while(file.available()){
-        Serial.write(file.read());
-    }
-    file.close();
-}
-
-void writeFile(fs::FS &fs, const char * path, const char * message){
-    Serial.printf("Writing file: %s\r\n", path);
-
-    File file = fs.open(path, FILE_WRITE);
-    if(!file){
-        Serial.println("- failed to open file for writing");
-        return;
-    }
-    if(file.print(message)){
-        Serial.println("- file written");
-    } else {
-        Serial.println("- write failed");
-    }
-    file.close();
-}
-
-void appendFile(fs::FS &fs, const char * path, const char * message){
-    Serial.printf("Appending to file: %s\r\n", path);
-
-    File file = fs.open(path, FILE_APPEND);
-    if(!file){
-        Serial.println("- failed to open file for appending");
-        return;
-    }
-    if(file.print(message)){
-        Serial.println("- message appended");
-    } else {
-        Serial.println("- append failed");
-    }
-    file.close();
-}
-#pragma endregion
-
-
-// read sensor value function
-void probe() {
-  int sensor = analogRead(3);
-  dataMessage = String(sensor) + "," + "\r\n";
-    // Note: the “\r\n” at the end ensures the next reading is written on the next line.
-  appendFile(SPIFFS, "/log.txt", dataMessage.c_str());
-  Serial.println(sensor);
-  }
-
-// define timers
-SSVTimer timer1;
-
-// define sensor and button
-#define LDR         3
-#define BUTTON      9
-int lastState = HIGH;
-int currentState;  
+// Current time
+unsigned long currentTime = millis();
+// Previous time
+unsigned long previousTime = 0; 
+// Define timeout time in milliseconds (example: 2000ms = 2s)
+const long timeoutTime = 2000;
 
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(115200);
-  pinMode(LDR, INPUT);
-  pinMode(BUTTON, INPUT_PULLUP);
-  // start SPIFFS
-  if(!SPIFFS.begin(FORMAT_SPIFFS_IF_FAILED)){
-        Serial.println("SPIFFS Mount Failed");
-        return;
-    }
-  writeFile(SPIFFS, "/log.txt", "Reading ID, Date, Hour, Temperature \r\n");
-    // Note: the “\r\n” at the end ensures the next reading is written on the next line.
-  listDir(SPIFFS, "/", 0);
-  
-  timer1.SetEnabled(true);
-  timer1.SetInterval(3*1000);
-  timer1.SetOnTimer(probe);
+  Wire.begin(SDA_1 , SCL_1 , I2C_Freq);
+  mcp.begin_I2C();
+  // Initialize the output variables as outputs
+  mcp.pinMode(outputmcp0, OUTPUT);
+  mcp.pinMode(outputmcp1, OUTPUT);
+  // Set outputs to LOW
+  mcp.digitalWrite(outputmcp0, LOW);
+  mcp.digitalWrite(outputmcp1, LOW);
+    delay(2000);                                // adding a delay, monitor need a bit more time to connect
 
-  delay(500);
+  // Connect to Wi-Fi network with SSID and password
+  Serial.println("Setting AP (Access Point)…");
+  // Remove the password parameter, if you want the AP (Access Point) to be open
+  //WiFi.softAP(ssid, password);
+  WiFi.softAP(ssid);
+  
+  // Print AccessPoint IP address and start web server
+  IPAddress IP = WiFi.softAPIP();
+  Serial.print("Page on this IP address: ");
+  Serial.println(IP);
+
+  server.begin();
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-  timer1.RefreshIt();
-  currentState = digitalRead(BUTTON);
-      if(lastState == LOW && currentState == HIGH) {
-        Serial.println("Button Pressed!");
-        readFile(SPIFFS, "/log.txt");
+void loop(){
+  WiFiClient client = server.available();   // Listen for incoming clients
+
+  if (client) {                             // If a new client connects,
+    currentTime = millis();
+    previousTime = currentTime;
+    Serial.println("New Client.");          // print a message out in the serial port
+    String currentLine = "";                // make a String to hold incoming data from the client
+    while (client.connected() && currentTime - previousTime <= timeoutTime) {  // loop while the client's connected
+      currentTime = millis();
+      if (client.available()) {             // if there's bytes to read from the client,
+        char c = client.read();             // read a byte, then
+        Serial.write(c);                    // print it out the serial monitor
+        header += c;
+        if (c == '\n') {                    // if the byte is a newline character
+          // if the current line is blank, you got two newline characters in a row.
+          // that's the end of the client HTTP request, so send a response:
+          if (currentLine.length() == 0) {
+            // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
+            // and a content-type so the client knows what's coming, then a blank line:
+            client.println("HTTP/1.1 200 OK");
+            client.println("Content-type:text/html");
+            client.println("Connection: close");
+            client.println();
+            
+            // turns the GPIOs on and off
+            if (header.indexOf("GET /0/on") >= 0) {
+              Serial.println("mcpGPIO 0 on");
+              outputmcp0State = "on";
+              mcp.digitalWrite(outputmcp0, HIGH);
+            } else if (header.indexOf("GET /0/off") >= 0) {
+              Serial.println("mcpGPIO 0 off");
+              outputmcp0State = "off";
+              mcp.digitalWrite(outputmcp0, LOW);
+            } else if (header.indexOf("GET /1/on") >= 0) {
+              Serial.println("mcpGPIO 1 on");
+              outputmcp1State = "on";
+              mcp.digitalWrite(outputmcp1, HIGH);
+            } else if (header.indexOf("GET /1/off") >= 0) {
+              Serial.println("mcpGPIO 1 off");
+              outputmcp1State = "off";
+              mcp.digitalWrite(outputmcp1, LOW);
+            }
+            
+            // Display the HTML web page
+            client.println("<!DOCTYPE html><html>");
+            client.println("<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
+            client.println("<link rel=\"icon\" href=\"data:,\">");
+            // CSS to style the on/off buttons 
+            // Feel free to change the background-color and font-size attributes to fit your preferences
+            client.println("<style>html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}");
+            client.println(".button { background-color: #4CAF50; border: none; color: white; padding: 16px 40px;");
+            client.println("text-decoration: none; font-size: 30px; margin: 2px; cursor: pointer;}");
+            client.println(".button2 {background-color: #555555;}</style></head>");
+            
+            // Web Page Heading
+            client.println("<body><h1>ESP32 Web Server</h1>");
+            
+            // Display current state, and ON/OFF buttons for MCP GPIO 0  
+            client.println("<p>MCP GPIO 0 - State " + outputmcp0State + "</p>");
+            // If the outputmcp0State is off, it displays the ON button       
+            if (outputmcp0State=="off") {
+              client.println("<p><a href=\"/0/on\"><button class=\"button\">ON</button></a></p>");
+            } else {
+              client.println("<p><a href=\"/0/off\"><button class=\"button button2\">OFF</button></a></p>");
+            } 
+               
+            // Display current state, and ON/OFF buttons for MCP GPIO 1  
+            client.println("<p>MCP GPIO 1 - State " + outputmcp1State + "</p>");
+            // If the outputmcp1State is off, it displays the ON button       
+            if (outputmcp1State=="off") {
+              client.println("<p><a href=\"/1/on\"><button class=\"button\">ON</button></a></p>");
+            } else {
+              client.println("<p><a href=\"/1/off\"><button class=\"button button2\">OFF</button></a></p>");
+            }
+            client.println("</body></html>");
+            
+            // The HTTP response ends with another blank line
+            client.println();
+            // Break out of the while loop
+            break;
+          } else { // if you got a newline, then clear currentLine
+            currentLine = "";
+          }
+        } else if (c != '\r') {  // if you got anything else but a carriage return character,
+          currentLine += c;      // add it to the end of the currentLine
+        }
       }
-  lastState = currentState;
+    }
+    // Clear the header variable
+    header = "";
+    // Close the connection
+    client.stop();
+    Serial.println("Client disconnected.");
+    Serial.println("");
+  }
 }
